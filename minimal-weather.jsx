4c0169ba518d8133a69754bf33222f06f195a88b
Jesse Seeligsohn <jesseseeligsohn@gmail.com>
2022-07-27 20:55:37 -0400

    Weather Widget created

---
+++ minimal-weather.jsx
@@ -0,0 +1,86 @@
+
+import { css } from "uebersicht"
+// import sun from './sun.png'
+
+// export const command = "echo Hello World!"
+
+
+const size = 30
+const top = 530
+const left = 660
+
+const units = "metric"
+const key = ""
+
+export const refreshFrequency = 30 * 60 * 1000 // ms
+
+export const initialState = {
+  loading: true,
+  temp: null,
+}
+
+// Get weather data
+export const command = (dispatch) => {
+  geolocation.getCurrentPosition(pos => {
+    const latitude = pos.position.coords.latitude
+    const longitude = pos.position.coords.longitude
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${units}&appid=${key}`
+    fetch(url)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        return dispatch({ data: data, type: "Success" })
+      })
+  })
+}
+
+export const updateState = (event, previousState) => {
+  switch (event.type) {
+    case 'Success':
+      return { ...previousState, temp: event.data.main.temp, loading: false, icon: event.data.weather[0].icon }
+    default: {
+      return previousState;
+    }
+  }
+}
+
+// const icon = 'minimal-weather.widget/icon.png'
+export const render = ({ loading, temp, icon }) => (
+  <div className={container}>
+    <img src={"minimal-weather.widget/icons/" + icon + ".png"} className={image} />
+    <p className={text}>{loading ? "Loading..." : Math.round(temp) + "°C"}</p>
+  </div>
+)
+
+export const className = {
+  top: top,
+  left: left,
+  color: '#fff',
+}
+
+const text = css`
+  font-size: ${size + "px"};
+  text-align: center;
+  color: white;
+  text-shadow:2px 2px 5px rgba(0,0,0,0.6);
+  font-family: 'Avenir', 'Ariel';
+  letter-spacing: 3px;
+  font-weight: lighter
+   font-smoothing: antialiased
+`
+
+const image = css`
+  align-self: center;
+  width: ${size + 25 + "px"};
+  length: ${size + 25 + "px"};
+  margin: 10px;
+
+  filter: drop-shadow(0px 0px 4px rgba(0,0,0,0.4));
+
+`
+
+const container = css`
+  display: flex;
+`
